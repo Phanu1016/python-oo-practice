454b930a9111f1a4cf7ab15dafe43198cadc9a74
Phanu <admin@phanuphanu.com>
2022-08-05 18:56:53 -0700

    finished

--- serial.py
+++ serial.py
@@ -19,4 +19,17 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+    def __init__(self, start):
+        """ Constructor for SerialGenerator """
+        self.start = self.current = start - 1
+    
+    def generate(self):
+        """ Generate a number base on the start value """
+        self.current += 1
+        return self.current
 
+    def reset(self):
+        """ Reset the current value to starting value """
+        self.current = self.start
+
+    
--- wordfinder.py
+++ wordfinder.py
@@ -1,5 +1,26 @@
-"""Word Finder: finds random words from a dictionary."""
-
+import random
 
 class WordFinder:
-    ...
+    """ Word Finder: finds random words from a dictionary. """
+    def __init__(self, path):
+        """ Constructor for WordFinder """
+        self.file = open(path, "r")
+        self.words = self.getAllWords()
+
+    def getAllWords(self):
+        """ Retrieves all words from a file """
+        return [line.strip() for line in self.file.readlines()]
+
+    def random(self):
+        """ Returns a random word from the file """
+        return self.words[random.randrange(0, len(self.words))]
+        
+class SpecialWordFinder(WordFinder):
+    """ Special Word Finder: finds random words from a dictionary not including spaces and # """
+    def __init__(self, path):
+        """ Constructor for SpecialWordFinder """
+        super().__init__(path)
+
+    def getAllWords(self):
+        """ Retrieves all words from a file (not reading spaces and anything starts with '#' """
+        return [line.strip() for line in self.file.readlines() if line.strip() != "" and not line.startswith("#")]
